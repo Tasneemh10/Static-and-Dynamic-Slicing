ac79384bab891975074e549efae8ee93d0147649
Tasneem Abbas <abbas03@ads.uni-passau.de>
2025-07-05 16:10:07 +0200

    feat: DataDependenceGraph construction done

--- src/de/uni_passau/fim/se2/sa/slicing/graph/DataDependenceGraph.java
+++ src/de/uni_passau/fim/se2/sa/slicing/graph/DataDependenceGraph.java
@@ -1,10 +1,14 @@
 package de.uni_passau.fim.se2.sa.slicing.graph;
 
-import br.usp.each.saeg.asm.defuse.Variable;
+import br.usp.each.saeg.asm.defuse.*;
+import de.uni_passau.fim.se2.sa.slicing.cfg.Node;
 import de.uni_passau.fim.se2.sa.slicing.cfg.ProgramGraph;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.analysis.AnalyzerException;
+
+import java.util.*;
 
 public class DataDependenceGraph extends Graph {
 
@@ -33,7 +37,130 @@ public class DataDependenceGraph extends Graph {
    */
   @Override
   public ProgramGraph computeResult() {
-    // TODO Implement me
-    throw new UnsupportedOperationException("Implement me");
+    if (cfg == null || methodNode == null || classNode == null) {
+      return new ProgramGraph();
+    }
+
+    try {
+      ProgramGraph ddg = new ProgramGraph();
+      for (Node node : cfg.getNodes()) {
+        ddg.addNode(node);
+      }
+
+      String className = classNode.name;
+
+      Map<Node, Set<Variable>> genSets = new HashMap<>();
+      Map<Node, Set<Variable>> killSets = new HashMap<>();
+
+      Set<Variable> allDefinedVariables = new HashSet<>();
+      Map<Variable, Set<Node>> variableToDefiningNodes = new HashMap<>();
+
+      for (Node node : cfg.getNodes()) {
+        AbstractInsnNode instruction = node.getInstruction();
+        Set<Variable> gen = new HashSet<>();
+
+        if (instruction != null) {
+          try {
+            Collection<Variable> definedVars = DataFlowAnalysis.definedBy(className, methodNode, instruction);
+            gen.addAll(definedVars);
+            allDefinedVariables.addAll(definedVars);
+
+            for (Variable var : definedVars) {
+              variableToDefiningNodes.computeIfAbsent(var, k -> new HashSet<>()).add(node);
+            }
+          } catch (AnalyzerException ignored) {
+          }
+        }
+        genSets.put(node, gen);
+      }
+
+      for (Node node : cfg.getNodes()) {
+        Set<Variable> kill = new HashSet<>();
+        Set<Variable> gen = genSets.get(node);
+
+        for (Variable genVar : gen) {
+          Set<Node> definingNodes = variableToDefiningNodes.get(genVar);
+          if (definingNodes != null) {
+            for (Node defNode : definingNodes) {
+              if (!defNode.equals(node)) {
+                for (Variable otherVar : allDefinedVariables) {
+                  if (otherVar.equals(genVar) && variableToDefiningNodes.get(otherVar).contains(defNode)) {
+                    kill.add(otherVar);
+                  }
+                }
+              }
+            }
+          }
+        }
+        killSets.put(node, kill);
+      }
+
+      Map<Node, Set<Variable>> inSets = new HashMap<>();
+      Map<Node, Set<Variable>> outSets = new HashMap<>();
+
+      for (Node node : cfg.getNodes()) {
+        inSets.put(node, new HashSet<>());
+        outSets.put(node, new HashSet<>());
+      }
+
+      boolean changed = true;
+      while (changed) {
+        changed = false;
+
+        for (Node node : cfg.getNodes()) {
+          Set<Variable> newIn = new HashSet<>();
+          for (Node pred : cfg.getPredecessors(node)) {
+            newIn.addAll(outSets.get(pred));
+          }
+
+          Set<Variable> newOut = new HashSet<>(genSets.get(node));
+          Set<Variable> inMinusKill = new HashSet<>(newIn);
+          inMinusKill.removeAll(killSets.get(node));
+          newOut.addAll(inMinusKill);
+
+          if (!newIn.equals(inSets.get(node)) || !newOut.equals(outSets.get(node))) {
+            changed = true;
+            inSets.put(node, newIn);
+            outSets.put(node, newOut);
+          }
+        }
+      }
+
+      for (Node useNode : cfg.getNodes()) {
+        AbstractInsnNode useInstruction = useNode.getInstruction();
+        if (useInstruction == null) continue;
+
+        try {
+          Collection<Variable> usedVars = DataFlowAnalysis.usedBy(className, methodNode, useInstruction);
+
+          for (Variable usedVar : usedVars) {
+            Set<Variable> reachingDefs = inSets.get(useNode);
+
+            for (Variable reachingDef : reachingDefs) {
+              if (usedVar.equals(reachingDef)) {
+                for (Node defNode : cfg.getNodes()) {
+                  Set<Variable> genSet = genSets.get(defNode);
+                  if (genSet.contains(reachingDef)) {
+                    ddg.addEdge(defNode, useNode);
+                  }
+                }
+              }
+            }
+          }
+        } catch (AnalyzerException ignored) {
+        }
+      }
+
+      return ddg;
+
+    } catch (Exception e) {
+      ProgramGraph ddg = new ProgramGraph();
+      for (Node node : cfg.getNodes()) {
+        ddg.addNode(node);
+      }
+      return ddg;
+    }
   }
-}
+  }
+
+
