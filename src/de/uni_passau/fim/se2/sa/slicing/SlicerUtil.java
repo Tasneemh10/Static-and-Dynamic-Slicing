b799b2b009cf55937b82d31bae23db01051c2984
Tasneem Abbas <abbas03@ads.uni-passau.de>
2025-07-08 22:45:20 +0200

    feat: dynamic analysis completed

--- src/de/uni_passau/fim/se2/sa/slicing/SlicerUtil.java
+++ src/de/uni_passau/fim/se2/sa/slicing/SlicerUtil.java
@@ -1,6 +1,16 @@
 package de.uni_passau.fim.se2.sa.slicing;
 
+import de.uni_passau.fim.se2.sa.slicing.cfg.Node;
+import de.uni_passau.fim.se2.sa.slicing.cfg.ProgramGraph;
+import de.uni_passau.fim.se2.sa.slicing.coverage.CoverageTracker;
 import de.uni_passau.fim.se2.sa.slicing.graph.ProgramDependenceGraph;
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+
+import java.util.Set;
 
 public class SlicerUtil {
 
@@ -11,8 +21,22 @@ public class SlicerUtil {
      * @param testCase  The name of the test case to be executed.
      */
     public static void executeTest(String className, String testCase) {
-        // TODO Implement execution of test method here
-        throw new UnsupportedOperationException("Execution of test method missing");
+        try {
+            CoverageTracker.reset();
+
+            Launcher junitLauncher = LauncherFactory.create();
+
+            String assumedTestClass = className + "Test";
+
+            LauncherDiscoveryRequest testRequest = LauncherDiscoveryRequestBuilder.request()
+                    .selectors(DiscoverySelectors.selectMethod(assumedTestClass, testCase))
+                    .build();
+
+            junitLauncher.execute(testRequest);
+        } catch (Exception ex) {
+            System.err.println("Test execution failed for " + testCase + " in " + className + ": " + ex.getMessage());
+            ex.printStackTrace();
+        }
     }
 
     /**
@@ -23,7 +47,30 @@ public class SlicerUtil {
      * @return The simplified program dependence graph.
      */
     public static ProgramDependenceGraph simplify(final ProgramDependenceGraph pPDG) {
-        // TODO Implement simplification of program-dependence graph for dynamic slicing
-        throw new UnsupportedOperationException("Simplification of PDG not implemented");
+        Set<Integer> coveredLines = CoverageTracker.getVisitedLines();
+
+        if (coveredLines.isEmpty()) {
+            return pPDG;
+        }
+
+        ProgramGraph fullGraph = pPDG.computeResult();
+        ProgramGraph reducedGraph = new ProgramGraph();
+
+        for (Node n : fullGraph.getNodes()) {
+            int ln = n.getLineNumber();
+            if (ln > 0 && coveredLines.contains(ln)) {
+                reducedGraph.addNode(n);
+            }
+        }
+
+        for (Node src : reducedGraph.getNodes()) {
+            for (Node dst : fullGraph.getSuccessors(src)) {
+                if (reducedGraph.getNodes().contains(dst)) {
+                    reducedGraph.addEdge(src, dst);
+                }
+            }
+        }
+
+        return new ProgramDependenceGraph(reducedGraph);
     }
 }
