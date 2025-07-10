5648c025f250b2db1ae7c4dfce8e24c9f31964f8
Tasneem Abbas <abbas03@ads.uni-passau.de>
2025-07-10 03:28:55 +0200

    update: removed unused functions in DataDependeceGraph

--- src/de/uni_passau/fim/se2/sa/slicing/graph/DataDependenceGraph.java
+++ src/de/uni_passau/fim/se2/sa/slicing/graph/DataDependenceGraph.java
@@ -194,14 +194,6 @@ public class DataDependenceGraph extends Graph {
     }
 
     @Override
-    public boolean equals(Object o) {
-      if (this == o) return true;
-      if (o == null || getClass() != o.getClass()) return false;
-      DefUse defUse = (DefUse) o;
-      return Objects.equals(node, defUse.node) && Objects.equals(variable, defUse.variable);
-    }
-
-    @Override
     public int hashCode() {
       return Objects.hash(node, variable);
     }
